623ce3a6a548cf264247a5a9d4b4f49b765777bd
KSherman97 <22063898+KSherman97@users.noreply.github.com>
2022-02-04 21:49:39 -0500

    scraping completed
    Can scrape and output to json in indexed format

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+import pandas as pd
+import sys
+import io
+
 def main():
-    print("test")
+
+    # Define the page url
+    url = 'https://en.wikipedia.org/wiki/2020_Summer_Olympics_medal_table'
+
+    # define the file I want to output to
+    output_file = './2022_medals.json'
+
+    # Define the variables I will use for storing the values from the table
+    rank = ""
+    NOC = ""
+    gold = 0
+    silver = 0
+    bronze = 0
+    total = 0
+
+    # Parse the HTML table from the webpage
+    df = pd.read_html(url)
+
+    # Set the dataframe to the result of the 2nd index
+    df = df[2]
+
+    # df = df.drop(columns=['Rank']) # drop the rank column
+    df.drop(df.tail(1).index,inplace=True) # drop the last row
+    
+    json = df.to_json(output_file, orient='index')
+
+    # print(df)
+    # print(df.columns)
 
 if __name__ == "__main__":
+    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
     main()
