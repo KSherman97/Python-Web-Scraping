de5401813b61dc55e3c60ff4c61138365fdb4d6b
KSherman97 <22063898+KSherman97@users.noreply.github.com>
2022-02-04 22:05:23 -0500

    Fixed a broken character issue

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Kyle Sherman
 The simple program was designed to scrape the Wikepedia page for the olympic games to get the current medal counter.
 I wanted a way to automate the collection of data since the API is locked behind a paywal
 
+url: https://en.wikipedia.org/wiki/2020_Summer_Olympics_medal_table?lang=en
+
 ## Requirements
 - Python
 - Pandas Library
--- main.py
+++ main.py
@@ -10,30 +10,32 @@ def main():
     # define the file I want to output to
     output_file = './2020_medals.json'
 
-    # Define the variables I will use for storing the values from the table
-    rank = ""
-    NOC = ""
-    gold = 0
-    silver = 0
-    bronze = 0
-    total = 0
-
     # Parse the HTML table from the webpage
-    df = pd.read_html(url)
+    data_frame = pd.read_html(url)
 
     # Set the dataframe to the result of the 2nd index
-    df = df[2]
+    data_frame = data_frame[2]
 
-    # df = df.drop(columns=['Rank']) # drop the rank column
-    df.drop(df.tail(1).index,inplace=True) # drop the last row
+    # data_frame = data_frame.drop(columns=['Rank']) # drop the rank column
+    data_frame.drop(data_frame.tail(1).index,inplace=True) # drop the last row
+
+    # There is a broken character in the name column for each country so fix it
+    data_frame['NOC'] = data_frame['NOC'].str.replace('\u00a0',' ')
     
-    json = df.to_json(output_file, orient='index')
+    # Output the dataframe to a JSON file oriented by index
+    json = data_frame.to_json(output_file, orient='index')
 
-    # print(df)
-    # print(df.columns)
+    # Uncomment these if you want to see what the dataframe looks like
+    ########################################################
+    # print(data_frame)
+    # print(data_frame.columns)
+    ########################################################
 
 if __name__ == "__main__":
-    sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
-    sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+    # This fixes formatting issues for output on Windows; uncomment if you're having issues
+    ########################################################
+    # sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+    # sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+    ########################################################
 
     main()
